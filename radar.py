63d1fc74d4e25f004c05ac73ab6f11fc577c2fde
Kevin McAleer <kevinmcaleer@gmail.com>
2022-11-06 16:17:50 +0000

    added radar_display

--- radar.py
+++ radar.py
@@ -12,8 +12,46 @@ from machine import Pin
 trigger_pin = 2
 echo_pin = 3
 
+DATA_FILE = 'readings.csv'
+
 s = Servo(0)
 r = RangeFinder(trigger_pin=trigger_pin, echo_pin=echo_pin)
+
+def take_readings(count):
+    readings = []
+    with open(DATA_FILE, 'ab') as file:
+        for i in range(0, 90):
+            s.value(i)
+            value = r.distance
+#             readings.append(value)
+            print(f'distance: {value}, angle {i} degrees, count {count}')
+            sleep(0.01)
+        for i in range(90,-90, -1):
+            s.value(i)
+            value = r.distance
+            readings.append(value)
+            print(f'distance: {value}, angle {i} degrees, count {count}')
+            sleep(0.01)
+        for item in readings:
+            file.write(f'{item}, ')
+        file.write(f'{count} \n')
+    
+    print('wrote datafile')
+    for i in range(-90,0,1):
+        s.value(i)
+        value = r.distance
+        print(f'distance: {value}, angle {i} degrees, count {count}')
+        sleep(0.05)
+
+def demo():
+    for i in range(-90, 90):
+        s.value(i)
+        print(f's: {s.value()}')
+        sleep(0.01)
+    for i in range(90,-90, -1):
+        s.value(i)
+        print(f's: {s.value()}')
+        sleep(0.01)
 
 def sweep(s,r):
     """ Returns a list of readings from a 180 degree sweep """
@@ -27,10 +65,13 @@ def sweep(s,r):
     return readings
 
 
-while True or not keyboard_interrupt:
-    
-    print(sweep(s,r))
+
+for count in range(1,2):
+    take_readings(count)
     sleep(0.25)
+    
+#     print(sweep(s,r))
+#     sleep(0.25)
     
 #     s.to_min()
 #     print(f'distance is: {r.distance}, {s}')
--- radar_display.py
+++ radar_display.py
@@ -3,15 +3,40 @@ import gc
 from math import sin, radians
 gc.collect()
 from time import sleep
+from range_finder import RangeFinder
+from machine import Pin
+from servo import Servo
+from motor import Motor
+
+m1 = Motor((4, 5))
+m1.enable()
+
+# run the motor full speed in one direction for 2 seconds
+m1.to_percent(100)
+
+trigger_pin = 2
+echo_pin = 3
+
+s = Servo(0)
+r = RangeFinder(trigger_pin=trigger_pin, echo_pin=echo_pin)
 
 display = PicoGraphics(DISPLAY_PICO_EXPLORER, rotate=0)
 WIDTH, HEIGHT = display.get_bounds()
 
+REALLY_DARK_GREEN = {'red':0, 'green':64, 'blue':0}
+DARK_GREEN = {'red':0, 'green':128, 'blue':0}
 GREEN = {'red':0, 'green':255, 'blue':0}
+LIGHT_GREEN = {'red':255, 'green':255, 'blue':255}
 BLACK = {'red':0, 'green':0, 'blue':0}
 
-black = display.create_pen(BLACK['red'], BLACK['green'], BLACK['blue'])
-green = display.create_pen(GREEN['red'], GREEN['green'], GREEN['blue'])
+def create_pen(display, color):
+    return display.create_pen(color['red'], color['green'], color['blue'])
+
+black = create_pen(display, BLACK)
+green = create_pen(display, GREEN)
+dark_green = create_pen(display, DARK_GREEN)
+really_dark_green = create_pen(display, REALLY_DARK_GREEN)
+light_green = create_pen(display, LIGHT_GREEN)
 
 length = HEIGHT //2
 middle = WIDTH // 2
@@ -40,16 +65,46 @@ def calc_vectors(angle, length):
     x2 = middle
     y2 = HEIGHT -1
     
-    print(f'a:{a}, b:{b}, c:{c}, A:{A}, B:{B}, C:{C}, angle: {angle}, length {length}, x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}')
+#     print(f'a:{a}, b:{b}, c:{c}, A:{A}, B:{B}, C:{C}, angle: {angle}, length {length}, x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}')
     return x1, y1, x2, y2
     
 a = 1
 while True:
-    x1, y1, x2, y2 = calc_vectors(a, 100)
+    
 #     print(f'x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}')
+    s.value(a)
+    distance = r.distance
+    if a > 1:
+        x1, y1, x2, y2 = calc_vectors(a-1, 100)
+        display.set_pen(really_dark_green)
+        
+        display.line(x1, y1, x2, y2)
+    
+    if a > 2:
+        x1, y1, x2, y2 = calc_vectors(a-2, 100)
+        display.set_pen(dark_green)
+        display.line(x1, y1, x2, y2)
+    
+#     if a > 3:
+#         x1, y1, x2, y2 = calc_vectors(a-3, 100)
+#         display.set_pen(black)
+#         display.line(x1, y1, x2, y2)
+       
+    # Draw the full length
+    x1, y1, x2, y2 = calc_vectors(a, 100)    
+    display.set_pen(light_green)
+    display.line(x1, y1, x2, y2)
+    
+    # Draw lenth as a % of full scan range (1200mm)
+    scan_length = int(distance * 3)
+    if scan_length > 100: scan_length = 100
+    print(f'Scan length is {scan_length}, distance is: {distance}')
+    x1, y1, x2, y2 = calc_vectors(a, scan_length)    
     display.set_pen(green)
     display.line(x1, y1, x2, y2)
+    
     display.update()
+
     a += 1
     if a > 180:
         a = 1
